f85976dea577e55acb41467ae117f3faf0f2277a
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-02-15 11:13:24 -0300

    Preparando revisão de código

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import './style/style.css'
-import './style/reset.css'
-
-ReactDOM.render(<App />, document.querySelector(".root"));
